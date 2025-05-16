da87cf3e3c5de326cbc4d0ac67c4ab828384019e
Muhammed Suhail P <suhailpalakkal1@gmail.com>
2025-05-16 16:15:37 +0530

    number added to each new items,  but will not adjust on deletin one item

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function newListItem(item)
      let d=document.createElement("div");
      d.classList.add("list-item");
     let p=document.createElement("p");
-    p.innerHTML=item;
+    let count=increment();
+    p.innerHTML=count+"."+item;
     let b1=document.createElement("button");
     b1.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive-fill" viewBox="0 0 16 16">
                     <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1M.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8z"/>
@@ -74,6 +75,18 @@ document.body.addEventListener("keydown",function(event){
     if(event.key=="Enter")
         add();
 });
+
+
+function makeCounter()
+{
+    let count=1;
+    return function()
+    {
+         return count++;
+    }
+}
+
+increment=makeCounter();
 
 
 
