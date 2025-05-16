bc16346ae538f63f27862fd3ed8134f6db50e4c9
Muhammed Suhail P <suhailpalakkal1@gmail.com>
2025-05-16 14:57:02 +0530

    new item created succesfully.

---
+++ index.js
@@ -0,0 +1,48 @@
+const { createElement } = require("react");
+
+function add()
+{
+    let newItem=document.getElementById("task-input").value;
+    if(newItem=="")
+        alert("please enter an item");
+    else 
+    newListItem(newItem);
+   
+    document.getElementById("task-input").value="";
+    
+
+}
+
+
+function newListItem(item)
+{
+     let d=document.createElement("div");
+    let p=document.createElement("p");
+    p.innerHTML=item;
+    let b1=document.createElement("button");
+    b1.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive-fill" viewBox="0 0 16 16">
+                    <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1M.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8z"/>
+                 </svg>`;
+    let b2=document.createElement("button");
+    b2.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+            <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+            </svg>`;
+
+    d2=document.createElement("div");
+    d2.appendChild(b1);
+    d2.appendChild(b2);
+
+    b1.classList.add("btn");
+    b2.classList.add("btn");
+    
+    b1.classList.add("btn-primary");
+    b2.classList.add("btn-primary");
+
+
+    d.appendChild(p);
+     d.appendChild(d2);
+    //   d.appendChild(b2);
+    document.getElementById("list-field").appendChild(d);
+    
+    
+}
