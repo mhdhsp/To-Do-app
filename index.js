795b734e109faa0e08d50469f5b0f86911e35d45
Muhammed Suhail P <suhailpalakkal1@gmail.com>
2025-05-19 12:23:38 +0530

    all set, but some styles to be addes in compleated task

--- index.js
+++ index.js
@@ -43,9 +43,20 @@ function redisplay()
          d1.appendChild(p2);
 
         //button div
+        d2=document.createElement("div");
+
         b1=document.createElement("button");
-       b1.innerHTML="delete";
+       b1.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive-fill" viewBox="0 0 16 16">
+                    <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1M.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8z"/>
+                 </svg>`;
        b1.classList.add("delete-btn");
+
+        b2=document.createElement("button");
+        b2.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+            <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+            </svg>`
+            d2.appendChild(b1);
+            d2.appendChild(b2);
 
 
             //function when clicking delete
@@ -57,10 +68,19 @@ function redisplay()
          
         this.parentElement.remove();
        redisplay();
-        // alert("hello");
+        
        });
 
-        d1.appendChild(b1);
+
+       b2.addEventListener("click",function(){
+        
+         this.parentElement.parentElement.classList.toggle("completed");
+
+       });
+
+
+
+        d1.appendChild(d2);
        document.getElementById("list-field").appendChild(d1);
 
 
@@ -101,19 +121,19 @@ function allclear()
 }
 
 
-// document.getElementById("outer").addEventListener("click",function(){
-//     // alert("hello");
-//     document.body.classList.toggle("dark-theme");
-//     document.getElementById("list-field").classList.toggle("dark-theme");
-//     document.getElementById("outer").classList.toggle("outer-dark");
-//     document.getElementById("inner").classList.toggle("inner-dark");
-//     them=document.getElementById("inner").textContent;
+document.getElementById("outer").addEventListener("click",function(){
+    // alert("hello");
+    document.body.classList.toggle("dark-theme");
+    document.getElementById("list-field").classList.toggle("dark-theme");
+    document.getElementById("outer").classList.toggle("outer-dark");
+    document.getElementById("inner").classList.toggle("inner-dark");
+    them=document.getElementById("inner").textContent;
     
     
-//     if(them=="Dark")
-//         them="Light";
-//     else
-//         them="Dark";
-//     document.getElementById("inner").innerHTML=them;
+    if(them=="Dark")
+        them="Light";
+    else
+        them="Dark";
+    document.getElementById("inner").innerHTML=them;
     
-// });
+});
